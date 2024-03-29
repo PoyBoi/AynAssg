7848998760bed2bceae612c84accf4733dfdd0a0
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-03-30 01:40:42 +0530

    fixed pathing issue, added pipe loader

--- scripts/pipeline_setup.py
+++ scripts/pipeline_setup.py
@@ -21,8 +21,22 @@ else:
     torch_dtype = torch.float32
 
 
-def pipelineSetup(model_path:int,clip_skip:int,):
-    text_encoder = os.path.join(model_path, "text_encoder")
+def pipelineSetup(model_path:int, prompt:str, clip_skip:int = 1):
+    # text_encoder = os.path.join(model_path, "text_encoder")
+    # print(text_encoder)
+    model_path = model_path.split("\\")
+    model_path = ["." if element == "AynAssg" else element for element in model_path]
+    model_path = "/".join(model_path) + '/'
+
+
+    if clip_skip > 1:
+        text_encoder = transformers.CLIPTextModel.from_pretrained(
+            model_path,
+            subfolder = "text_encoder",
+            num_hidden_layers = 12 - (clip_skip - 1),
+            torch_dtype = torch_dtype
+        )
+
     if clip_skip != 1:
         # TODO clean this up with the condition below.
         pipe = diffusers.DiffusionPipeline.from_pretrained(
@@ -35,5 +49,16 @@ def pipelineSetup(model_path:int,clip_skip:int,):
         pipe = diffusers.DiffusionPipeline.from_pretrained(
             model_path,
             torch_dtype = torch_dtype,
-            safety_checker = None
+            safety_checker = None,
+            # repo_type = 
         )
+
+    pipe = pipe.to(device_name)
+
+    # Change the pipe scheduler to EADS.
+    pipe.scheduler = diffusers.EulerAncestralDiscreteScheduler.from_config(
+        pipe.scheduler.config
+)
+    
+
+# TODO Need to get this location thing working, this is a pain in the butt
